d94e7ae651fd81b71da6dd999e4654d9b1ba6445
kevly <93ly.kevin@gmail.com>
2019-09-24 12:57:47 -0700

    Fix user auth registration

---
+++ models/Schedule.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema();
+
+const ScheduleSchema = new Schema({
+    pack_id: {
+        type: Number,
+        required: true
+    },
+
+    
+})
