2886c048d9e061d0ab11224098ccb3fe474209ee
JustinLieu06 <justinlieu06@gmail.com>
2019-09-23 17:48:24 -0700

    fixed frontend

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <p>TravelPack</p>
+        <footer>
+          Copyright &copy; 2019 TravelPack
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
