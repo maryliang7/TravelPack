1b523fc4f49f5e34d8f72c7b3ccc193d1c57f1b3
kevly <93ly.kevin@gmail.com>
2019-09-24 13:21:23 -0700

    Fix user auth registration/login

--- routes/api/users.js
+++ routes/api/users.js
@@ -45,6 +45,21 @@ router.post('/register', (req, res) => {
               newUser.password = hash;
               newUser.save()
                 .then(user => res.json(user))
+                .then(user => {
+                  const payload = { id: user.id, name: user.name };
+
+                  jwt.sign(
+                    payload,
+                    keys.secretOrKey,
+                    // Tell key to expire in one hour
+                    { expiresIn: 3600 },
+                    (err, token) => {
+                      res.json({
+                        success: true,
+                        token: 'Bearer ' + token
+                      });
+                    });
+                })
                 .catch(err => console.log(err));
             })
           })
@@ -74,7 +89,7 @@ router.post('/register', (req, res) => {
         bcrypt.compare(password, user.password)
         .then(isMatch => {
             if (isMatch) {
-            const payload = {id: user.id, name: user.name};
+            const payload = {id: user.id, email: user.email};
 
             jwt.sign(
                 payload,
