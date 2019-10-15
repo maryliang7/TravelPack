420885b8421a683b34fca32c6011b557ffc7fc7e
kevly <93ly.kevin@gmail.com>
2019-10-14 17:54:16 -0700

    Prepare for next pull

--- frontend/src/components/schedule/schedule.js
+++ frontend/src/components/schedule/schedule.js
@@ -125,7 +125,7 @@ class Schedule extends React.Component{
                                   />
                               </li>
                             ))}
-                            {(this.state.addSchedule === true ? this.displayNewScheduleForm() : this.displayAddScheduleButton())}
+                            {this.state.addSchedule === true ? this.displayNewScheduleForm() : this.displayAddScheduleButton()}
                             {/* <div className="new-schedule-button">
                               <Link className="new-schedule-link" to={`/packs/${this.props.pack._id}/schedules/new`}>
                                 <i className="far fa-calendar-plus">&nbsp;&nbsp;</i>
