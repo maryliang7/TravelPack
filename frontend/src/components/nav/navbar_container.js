bda410550fa2276655d91fece31dba24d81565cb
Mary Liang <mary.liang96@gmail.com>
2019-10-21 17:37:40 -0700

    add active class to packs

--- frontend/src/components/nav/navbar_container.js
+++ frontend/src/components/nav/navbar_container.js
@@ -4,12 +4,12 @@ import { logout } from '../../actions/session_actions';
 import { getUserPacks, removePacks } from '../../actions/pack_actions';
 import NavBar from './navbar';
 
-const mapStateToProps = state => {
+const mapStateToProps = (state, ownProps) => {
   let loggedIn = state.session.isAuthenticated;
   let currentUser = state.session.user;
   let packs = Object.values(state.entities.packs);
 
-  return { loggedIn, currentUser, packs }
+  return { loggedIn, currentUser, packs, ownProps }
 };
 
 const mapDispatchToProps = (dispatch) => ({
